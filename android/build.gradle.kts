2745db0e060a418649b826e346b3b8192a9e6ff2
Ansh Malik <thisisanshmalik@gmail.com>
2025-07-29 08:25:26 +0530

    Android Issue fixed

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
         classpath("com.google.gms:google-services:4.4.0")
     }
 }
